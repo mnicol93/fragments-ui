e3fcc4a70a1b1f7fa9eb338659bbd167c301d9a9
Marc Nicolas Oliva <marc.nicolasoliva@hotmail.com>
2022-08-19 03:43:28 -0400

    App running POST and GET

--- src/api.js
+++ src/api.js
@@ -6,19 +6,45 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
  * fragments microservice (currently only running locally). We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
-export async function getUserFragments(user) {
+export async function getUserFragments(user, expand=0) {
   console.log('Requesting user fragments data...');
   try {
-    const res = await fetch(`${apiUrl}/v1/fragments`, {
+    const res = await fetch(`${apiUrl}/v1/fragments?expand=${expand}`, {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
+
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
     console.log('Got user fragments data', { data });
+    return { data };
   } catch (err) {
     console.error('Unable to call GET /v1/fragment', { err });
+    return null;
+  }
+}
+export async function postFragment(user, value, contentType){
+  try{
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: 'POST',
+      headers:{
+        Authorization: `Bearer ${user.idToken}`,
+        'Content-Type': contentType
+      },
+      body: value
+    });
+    console.log(res);
+    if(!res.ok){
+      const error = await res.json();
+      throw error.error?.message || res.statusText;
+    }
+    const data = await res.json();
+    console.log('Posted fragments data', { data });
+    return data;
+  }catch(err){
+    console.error('Unable to call POST ', { err });
+    throw new Error(err);
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import { Auth, getUser } from './auth';
-import { getUserFragments } from './api';
+import { getUserFragments, postFragment } from './api';
 
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
+  // Action to be performed on the Fragment (Get, Create, Delete)
+  const fragmentAction = document.querySelector('.fragment-action');
+  // List of current Fragments (Get, Delete)
+  const fragmentList = document.querySelector('.select-fragment');
+  // Form to create a new Fragment
+  const fragmentForm = document.querySelector('.create-fragment-form');
+  // DIV containing fragment manipulation. Add HTML programmatically if user is logged in
+  const divFragmentForm = document.querySelector('.form');
+  // File uploader
+  const fileUpload = document.querySelector('#file-upload-input');
+  // Content-type selected
+  const typeUpload = document.querySelector('#content-type-form');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -18,18 +30,19 @@ async function init() {
     // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
     Auth.signOut();
   };
-
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
   if (!user) {
     // Disable the Logout button
     logoutBtn.disabled = true;
     return;
+  }else{
+    divFragmentForm.hidden=false;
   }
-
+  
   // Log the user info for debugging purposes
   console.log({ user });
-
+  //let results = await getUserFragments(user, 1);
   // Update the UI to welcome the user
   userSection.hidden = false;
 
@@ -39,8 +52,68 @@ async function init() {
   // Disable the Login button
   loginBtn.disabled = true;
 
-  // Do an authenticated request to the fragments API server and log the result
-  getUserFragments(user);
+    // Every time an item is selected from the list, function will trigger with
+  // event holding properties of the change (i.e, selected value)
+  fragmentAction.onchange = async (event) => {
+    if(event.target.value === ''){
+      fragmentList.hidden=true;
+      fragmentForm.hidden=true;
+    }else if(event.target.value == 'create'){
+      // Hide List, we are creating a new Fragment
+      fragmentList.hidden=true;
+      fragmentForm.hidden=false;
+    }else{
+      // Show List
+      fragmentList.hidden=false;
+      fragmentForm.hidden=true;
+      if(event.target.value == 'get'){
+        await getUserFragments(user, 1);
+      }
+    }
+  }
+  fileUpload.addEventListener('change', handleChange);
+  function handleChange(event){
+    const uploadedFiles = event.target.files;
+    let file = uploadedFiles[0];
+    let fileNames = uploadedFiles[0].name;
+    let typesNonMatching="";
+
+    //Populate array with filenames for user to confirm and show message
+    for(i=1; i < uploadedFiles.length; i++){
+      fileNames += ", " + uploadedFiles[i].name;
+    }
+    let fragment;
+
+    const reader = new FileReader();
+
+    if(!confirm(
+        "Files uploaded:\n" + fileNames + "\nType: " + typeUpload.value
+        + "\nWould you like to proceed?"
+      )){
+      console.log("Uploaded cancelled, please upload new files.")
+      return;
+    }
+
+    reader.onload = (function() {
+      return async function(event){
+        console.log("Attempting to POST... Wait please");
+        try{
+          fragment = await postFragment(user, event.target.result, file.type)
+        }catch(err){
+          console.log(err)
+        }
+      }
+    })(file);
+    reader.readAsArrayBuffer(file);
+
+    // Inform the user if some of the files weren't a type-match.
+    if(typesNonMatching.length > 0){
+      alert('The following files could not be uploaded, TYPE NOT MATCHING:\n' + typesNonMatching);
+    }
+  }
+
+  // Do an authenticated req to the fragments API server and log the result
+  let results = getUserFragments(user);
 }
 
 // Wait for the DOM to be ready, then start the app
